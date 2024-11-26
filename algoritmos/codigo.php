413b7cda575819d80411098f4bb3e07b3951923e
kevinMaga <kerimaga@espol.edu.ec>
2024-11-25 21:38:10 -0500

    AnalizadorSemantico-ValidacionDeTiposDeDatos

---
+++ algoritmos/codigo.php
@@ -0,0 +1,7 @@
+<?php
+$edad = 25;  // Correcto
+$edad = "treinta";  // Error: Asignación incompatible
+$nombre = "Juan";
+$resultado = $edad + $nombre;  // Error: Operación no válida
+echo $edad . $nombre;  // Correcto
+?>
