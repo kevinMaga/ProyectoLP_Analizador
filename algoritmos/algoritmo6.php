45cfc300679c7af8ff3ec42744b9b41877eefc6e
Ariana Gonzabay <arigalda@espol.edu.ec>
2024-11-27 23:34:29 -0500

    cambios en el algoritmo6

--- algoritmos/algoritmo6.php
+++ algoritmos/algoritmo6.php
@@ -1,48 +1,37 @@
 <?php
-// Inicio Ariana Gonzabay
-
 // Diccionario
-$diccionario = [
-    "nombre" => "Ariana",
-    "edad" => 22,
-    "universidad" => "ESPOL"
+$diccionario = [ "nombre" => "Ariana", "edad" => 22, "universidad" => "ESPOL"
 ];
 print_r($diccionario);
 
-// Estructura While
+# Estructura While
 $contador = 0;
 while ($contador < 3) {
     echo "Contador: $contador\n";
     $contador++;
 }
-
 // Condición simple
 $numero1 = 5;
 $numero2 = 10;
 if ($numero1 < $numero2) {
     echo "$numero1 es menor que $numero2\n";
 }
-
 // Condición compleja
 $numero3 = 15;
 if ($numero1 < $numero2 &&  $numero2 < $numero3) {
     echo "$numero1 es menor que $numero2 y $numero2 es menor que $numero3\n";
 }
-
 // Función anónima
 $saludar = function ($nombre) {
     echo "Hola, $nombre!\n";
 };
 
-
-// Impresión con múltiples argumentos
-echo "Impresión con múltiples argumentos:\n";
-echo "Este es un mensaje con varios argumentos\n";
-
 // Solicitud de datos
 echo "Ingrese su nombre: ";
 $nombre = readline();
 echo "Hola, $nombre!\n";
-
-// Fin Ariana Gonzabay
+/* Este es un 
+comentario de
+varias lineas*/
 ?>
+
